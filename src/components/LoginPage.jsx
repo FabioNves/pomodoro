dcfe1ae5732f123c25949f1a17f8608fa8944d10
Fabio <fabionevez@outlook.pt>
2025-04-18 15:38:02 -0300

    change name

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,8 +3,8 @@ import GoogleAuth from "./GoogleAuth";
 
 const LoginPage = ({ onLoginSuccess }) => {
   return (
-    <div className="login-page flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">Welcome to Pomodoro App</h1>
+    <div className="login-page flex flex-col items-center justify-center h-screen bg-gray-800">
+      <h1 className="text-4xl font-bold mb-6">Welcome to POMODRIVE</h1>
       <p className="mb-4">Please log in to continue</p>
       <GoogleAuth onLogin={onLoginSuccess} />
     </div>
